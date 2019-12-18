7ed9cd2821c3ae5d34474469915f48e799c809d5
cindy <cindy.zhang@thoughtworks.com>
2019-12-18 13:36:15 +0800

    remove unused code

--- src/main/scala/citibike/FileReader.scala
+++ src/main/scala/citibike/FileReader.scala
@@ -18,16 +18,6 @@ object FileReader {
     val stationsPath = "user/hdfs/wiki/stations1"
     val cityPath = "user/hdfs/wiki/city_info"
 
-    val stationStruct = (new StructType)
-      .add("id", StringType, false)
-      .add("name", StringType, false)
-      .add("free_bikes", IntegerType, false)
-      .add("empty_slots", IntegerType, false)
-      .add("latitude", DoubleType, false)
-      .add("longitude", DoubleType, false)
-      .add("extra", StringType, true)
-      .add("timestamp", StringType, false)
-
     val sdf_parquet = spark.read.parquet(hdfs_master + stationsPath)
 
     sdf_parquet.show()
